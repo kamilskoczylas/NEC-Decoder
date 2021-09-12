8396808113a16c2672d4aaad4fb12615435ba204
Kamil <kamilskoczylas@o2.pl>
2021-09-12 17:19:00 +0200

    Update Example.py

--- Example.py
+++ Example.py
@@ -1,8 +1,11 @@
 # Program start
 import NecDecoder
 
+GPIO_Mode = GPIO.BCM
+GPIO_PIN = 16
+
 # Example
-IReader = IRdecoder()
+IReader = IRdecoder(GPIO_Mode, GPIO_PIN)
 
 while True:
     sleep(0.1)
