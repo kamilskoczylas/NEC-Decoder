05a64b73207d2f5f57e0e63ea87dd816211d9b0d
Kamil <kamilskoczylas@o2.pl>
2024-06-29 21:09:25 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,9 +1,6 @@
 # Tests using text file data
 
-# Only for value of GPIO.BCM
 import RPi.GPIO as GPIO 
-# Sleeping allows CPU to increase performance for other applications on Raspberry
-# CPU goes down from 100% to 5-15% on NECDecoder
 from time import sleep 
 
 from SignalDecoder import SignalDecoder
@@ -13,7 +10,7 @@ GPIO_PIN = 16
 
 # Initialization of the class. Sets thread deamon
 # Default values are GPIO.BCM and PIN 16
-IReader = SignalDecoder(GPIO_Mode, GPIO_PIN)
+IReader = SignalDecoder(GPIOEdgeDetectedDataProvider(GPIO_Mode, GPIO_PIN))
 
 while True:
     sleep(0.1)
