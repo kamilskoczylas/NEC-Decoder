bf8be32b63313055ccd5930e82823329b3a13429
Kamil <kamilskoczylas@o2.pl>
2024-06-30 21:17:05 +0200

    Create GPIOEdgeDetectedDataProvider.py

---
+++ GPIOEdgeDetectedDataProvider.py
@@ -0,0 +1,39 @@
+#
+#   GPIOEdgeDetectedDataProvider.py
+#   Designed for Raspberry Pi, Python 3
+#
+#   2021-2024 Kamil Skoczylas
+#   MIT Licence
+#
+
+from abc import ABC, abstractmethod
+from timeit import default_timer
+from queue import Queue
+from queue import Empty
+import RPi.GPIO as GPIO
+
+class GPIOEdgeDetectedDataProvider(ABC):
+    
+    def __init__(self, GPIO_Mode = None, GPIO_PIN = None):
+        
+        if not GPIO_Mode is None:
+            self.GPIO_Mode = GPIO_Mode
+
+        if not GPIO_PIN is None:    
+            self.GPIO_PIN = GPIO_PIN
+            
+        GPIO.setmode(self.GPIO_Mode)
+        GPIO.setup(self.GPIO_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP) 
+        pass
+    
+    def SignalEdgeDetected(self, PinNumber):
+        self.Queue.put_nowait(default_timer())
+        pass
+        
+    def InitDataQueue(self, queue):
+        self.Queue = queue
+        GPIO.add_event_detect(self.GPIO_PIN, GPIO.FALLING, callback = self.SignalEdgeDetected)
+        pass
+
+    def __del__(self):
+        GPIO.cleanup(self.GPIO_PIN)
