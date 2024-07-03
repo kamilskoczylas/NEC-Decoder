d2601162c6f78289377cf85e3238b3029ff1988d
Kamil <kamilskoczylas@o2.pl>
2024-07-03 22:19:01 +0200

    Update Example.py

--- Example.py
+++ Example.py
@@ -9,9 +9,13 @@ import GPIODataProvider
 GPIO_Mode = GPIO.BCM
 GPIO_PIN = 16
 
-# Initialization of the class. Sets thread deamon
-# Default values are GPIO.BCM and PIN 16
-IReader = SignalDecoder.SignalDecoder(GPIODataProvider.EdgeDetected(GPIO_Mode, GPIO_PIN), NEC.NECDecoder())
+IReader = SignalDecoder.SignalDecoder(
+    GPIODataProvider.EdgeDetected(
+        GPIO_Mode,
+        GPIO_PIN
+    ),
+    NEC.NECDecoder()
+    )
 
 while True:
     sleep(0.1)
