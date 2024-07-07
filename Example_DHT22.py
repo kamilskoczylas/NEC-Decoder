f1d986a1078a517d107538a637673fb980b40bef
Kamil <kamilskoczylas@o2.pl>
2024-07-07 21:11:48 +0200

    Update Example_DHT22.py

--- Example_DHT22.py
+++ Example_DHT22.py
@@ -17,19 +17,17 @@ IReader = SignalDecoder.SignalDecoder(
     DHT22.DHT22Decoder()
     )
 
+# Keep positive signal for a while
+GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP) 
+sleep(2)
 
-
-# waiting
-while not IReader.hasDetected():
-    # Keep positive signal for a while
-    GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP) 
-    sleep(2)
-    
-    # You have to pull negative signal for at least 1 ms to request data from DHT22
-    GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN) 
-    sleep(0.005)
+# You have to set negative signal for at least 1 ms to request data from DHT22
+GPIO.setup(GPIO_PIN, GPIO.OUT)
+GPIO.output(GPIO_PIN, GPIO.LOW)
+sleep(0.005)
 
 cmd = IReader.getCommand()
 print(cmd)
 
+GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP) 
 
