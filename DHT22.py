f8e8ac03bc31f720b80bf553ba1e0305dacd6c9f
Kamil <kamilskoczylas@o2.pl>
2024-07-10 22:55:20 +0200

    Update DHT22.py

--- DHT22.py
+++ DHT22.py
@@ -116,7 +116,8 @@ class DHT22Decoder:
           signalTime = edgeTimeDetected - self.currentSignalStartTime
           self.currentSignalStartTime = edgeTimeDetected
 
-          print(signalTime)
+          if DEBUG:
+            print(signalTime)
           
           # If signal starts 13,5ms
           if signalTime > 0.002 and signalTime < 0.008:
