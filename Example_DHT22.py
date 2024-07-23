32c42ffc2bdf576b309205d54174c93efc061eec
Kamil <kamilskoczylas@o2.pl>
2024-07-23 22:26:07 +0200

    Update Example_DHT22.py

--- Example_DHT22.py
+++ Example_DHT22.py
@@ -4,8 +4,8 @@ import TemperatureSensor
 
 GPIO_PIN = 12
 
-# DHT22 connected to PIN 12 of Raspberry (BCM), class refresh every 2 seconds
-Sensor = TemperatureSensor.TemperatureSensor(GPIO_PIN, 2)
+# DHT22 connected to PIN 12 of Raspberry (BCM), class refresh every 4 seconds
+Sensor = TemperatureSensor.TemperatureSensor(GPIO_PIN, 4)
 
 while True:
     print("Temperature = {temperature}°C, Humidity = {humidity}%".format(temperature=Sensor.Temperature, humidity=Sensor.Humidity))
