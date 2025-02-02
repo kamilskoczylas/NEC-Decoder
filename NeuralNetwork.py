4f76fc667da4cfc1dfa6a4e3023aef8c4f1ceba0
Kamil Skoczylas <programowanie.vsz@gmail.com>
2025-02-02 11:26:56 +0100

    Enhance Neural Validators

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -181,9 +181,9 @@ class NeuralValue(ABC):
 		for i in range(0, self.max_bits):
 			self.neuralBits[i].calculate()	
 		for i in range(0, self.max_bits):
-			if i < self.value_bits or (i == self.max_bits - 1 and self.is_signed):
+			if i < self.value_bits or ((i == (self.max_bits - 1)) and self.is_signed):
 				if round(self.neuralBits[i].value) == 1:
-					if i == self.max_bits - 1 and self.is_signed:
+					if (i == (self.max_bits - 1)) and self.is_signed:
 						multiply_by = -1
 					else:
 						self.value += 1 << i
