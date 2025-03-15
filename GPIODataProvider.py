ebbef0530dc16b8b4b4a9ca857b17695ab13c838
Kamil Skoczylas <programowanie.vsz@gmail.com>
2025-03-15 21:38:44 +0100

    Enhance spead of reading from GPIO

--- GPIODataProvider.py
+++ GPIODataProvider.py
@@ -58,6 +58,7 @@ class EdgeDetected(SignalDataProvider):
 
 			last_signal_state = signal_state
 			reading_time = detected_beginning_of_signal_time
+			last_reading_time = detected_beginning_of_signal_time
 			i = 0
    
 			# Reading the signal until its end, or end of time it should end
@@ -72,8 +73,9 @@ class EdgeDetected(SignalDataProvider):
 				if last_signal_state != signal_state and signal_state == GPIO.LOW:
 					if i != 0:
 						reading_time = default_timer()
-					print("{:.5f}".format(reading_time - detected_beginning_of_signal_time))
+					print("{:.5f}".format(reading_time - last_reading_time))
 					self.Queue.put(reading_time)
+					last_reading_time = reading_time
      
 				last_signal_state = signal_state
     
